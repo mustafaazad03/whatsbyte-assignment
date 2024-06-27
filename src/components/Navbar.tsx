16b270212fa80da554ffd4c7883ef38c90598ca2
Mustafa Azad <mustafaazad533@gmail.com>
2024-06-27 12:49:52 +0530

    chore: Update Navbar component styles for responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<div className="bg-white border-b border-gray-200 p-8 py-6 flex items-center justify-between fixed w-full z-50">
+		<div className="bg-white border-b border-gray-200 sm:p-8 py-6 px-4 flex items-center justify-between fixed w-full z-50">
 			<div className="h-full w-auto">
 				<Image
 					src="/logo.png"
 					alt="logo"
 					width={140}
 					height={140}
-					className="object-cover w-48 h-full"
+					className="object-cover sm:w-48 w-28 h-full"
 				/>
 			</div>
 			<div className="rounded-lg p-2 flex items-center space-x-2 border-2 border-gray-200">
@@ -19,9 +19,9 @@ const Navbar = () => {
 					alt="profile"
 					width={50}
 					height={50}
-					className="uneven-border-radius w-9 h-8 object-cover"
+					className="uneven-border-radius sm:w-9 sm:h-8 w-5 h-4 object-cover"
 				/>
-				<h3 className="font-bold">Rahil Siddique</h3>
+				<h3 className="font-bold sm:text-base text-sm">Rahil Siddique</h3>
 			</div>
 		</div>
 	);
